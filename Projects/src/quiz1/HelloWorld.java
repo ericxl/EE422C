3f980dbabece7d2ef67d1cde41945d1d1a4d9b1d
Eric Liang <jklelxy@gmail.com>
2016-08-31 16:37:53 -0500

    added quiz, reorginzed folders

---
+++ Projects/src/quiz1/HelloWorld.java
@@ -0,0 +1,32 @@
+// HelloWorld 
+/* 
+ * EE422C Quiz 1 submission by  
+ * Replace <...> with your actual data.  
+ * Xiaoyong Liang
+ * XL5432
+ * 16445  
+ * Fall 2016  
+ */
+package quiz1;
+
+/**
+ * A HelloWorld class.
+ *
+ */
+public class HelloWorld {
+	
+	/**
+	 * Prints "Hello World" on the console.
+	 */
+	public static void hello(){
+		System.out.println("Hello World");
+	}
+	
+	/**
+	 * Prints the message specified on the console.
+	 * @param msg is the message to print
+	 */
+	public static void printMsg(String msg) {
+		System.out.println(msg);
+	}
+}
