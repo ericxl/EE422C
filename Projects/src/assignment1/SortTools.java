a93d096360d5e462190d900f67ecb7c895c50c5c
Eric Liang <jklelxy@gmail.com>
2016-09-01 18:52:27 -0500

    use intelliJ

--- Projects/src/assignment1/SortTools.java
+++ Projects/src/assignment1/SortTools.java
@@ -52,19 +52,102 @@ public class SortTools {
 	 * @return
 	 */
 	public static int find(int[] x, int n, int v) {
-		return 1;
+		if (x == null)
+			return -1;
+		int len = n;
+		if (x.length < len) {
+			len = x.length;
+		}
+		if (len <= 0) {
+			return -1;
+		}
+
+		int upperBound = len - 1;
+		int current = (upperBound + (upperBound % 2)) / 2;
+		while (upperBound - current > 1) {
+			if (x[current] == v) {
+				return current;
+			}
+			if (x[current] > v) {
+				// should search in lower half
+				upperBound = current;
+				current = (upperBound + (upperBound % 2)) / 2;
+			} else {
+				// should search in upper half
+				int boundSum = upperBound + current;
+				current = (boundSum + (boundSum % 2)) / 2;
+			}
+		}
+		if (x[current] == v) {
+			return current;
+		}
+		if (x[upperBound] == v) {
+			return upperBound;
+		}
+		return -1;
 	}
 
 	public static int[] insertGeneral(int[] x, int n, int v) {
+		if (x == null)
+			return new int[] { v };
+		int len = n;
+		if (x.length < len) {
+			len = x.length;
+		}
+		if (len <= 0) {
+			return new int[] { v };
+		}
+
+		int originalArrayCopyIndex = 0;
+		int newArrayIndex = 0;
+		int[] newArray = new int[n + 1];
+
+		int i = 0;
+		while (originalArrayCopyIndex < newArray.length) {
+
+		}
 		return null;
 	}
 
+	/**
+	 * Inserts v into array for the first n elements.
+	 * @param x is the array to be inserted.
+	 * @param n is the number of elements to be inserted into.
+	 * @param v is the value to be inserted.
+	 * @return
+	 */
 	public static int insertInPlace(int[] x, int n, int v) {
 		return 1;
 	}
 
+	/**
+	 * Sorts the first n elements of an array in a non decreasing order.
+	 * @param x is the array to be sorted.
+	 * @param n is the number of elements to be sorted.
+	 */
 	public static void insertSort(int[] x, int n) {
-
+		if(x == null){
+			return;
+		}
+		int len = n;
+		if (x.length < len) {
+			len = x.length;
+		}
+		if (len <= 0) {
+			return;
+		}
+		
+		boolean dirty = true;
+		while (dirty) {
+			dirty = false;
+			for (int j = 0; j < len - 1; j++) {
+				if (x[j] > x[j + 1]) {
+					int temp = x[j];
+					x[j] = x[j + 1];
+					x[j + 1] = temp;
+					dirty = true;
+				}
+			}
+		}
 	}
-
 }
